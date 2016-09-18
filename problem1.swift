5a10a0706cd8b828b4cef17a3a83f7ba7597ebf3
Donald Pinckney 2 <djpinckney@ucdavis.edu>
2016-09-18 03:05:31 +0000

    Implement problem 1

--- problem1.swift
+++ problem1.swift
@@ -1,4 +1,7 @@
-import Foundation
+// See problem1.README
 
-// For this problem, you need to print the text "Hello, World!"
-print("my text here")
+// Note that the elements in this array are unique
+let data = [19, 524, 95, 627, 230, 960, 150, 880, 659, 724, 420, 215, 470, 787, 883, 269, 678, 718, 329, 355]
+
+let wantToFind = Int(readLine()!)!
+
