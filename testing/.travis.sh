89d17ee1b2ba730416f938b024749ec1ad41c3a0
Donald Pinckney <donald_pinckney@icloud.com>
2016-09-22 18:47:12 -0700

    Update .travis.sh

--- testing/.travis.sh
+++ testing/.travis.sh
@@ -2,15 +2,10 @@
 
 # Don't call this script yourself, this is for Travis CI!
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    exit 0
-elif [ -z ${TRAVIS_PULL_REQUEST+x} ]; then
-    exit 0
-fi
-
 cd "$(dirname "$0")"/..
 
 testing/test_all.sh
 exitCode=$?
+#testing/.upload_results.sh
 
 exit $exitCode
