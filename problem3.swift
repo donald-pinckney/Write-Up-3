762398578f71292e465792b92b98e9756604d116
Donald Pinckney 2 <djpinckney@ucdavis.edu>
2016-09-29 05:52:35 +0000

    Add lots of problems

---
+++ problem3.swift
@@ -0,0 +1,9 @@
+// See problem2.README
+
+// This loop reads in the array of input numbers
+var inputArray: [Int] = []
+while let inputNum = Int(readLine()!) {
+	inputArray.append(inputNum)
+}
+
+// Write your balancing code here.
