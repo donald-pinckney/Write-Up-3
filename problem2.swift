1342dbf528ac88b0b7704f6d2cabcf9ee7955f43
Donald Pinckney 2 <djpinckney@ucdavis.edu>
2016-09-29 07:25:34 +0000

    Implement problem 3

--- problem2.swift
+++ problem2.swift
@@ -1,9 +1,11 @@
 // See problem2.README
 
-// This loop reads in the array of input numbers
+// This loop reads in the array of inputted positive integers
 var inputArray: [Int] = []
 while let inputNum = Int(readLine()!) {
 	inputArray.append(inputNum)
 }
 
 // Write your balancing code here.
+
+
--- problem3.swift
+++ problem3.swift
@@ -1,9 +1,19 @@
-// See problem2.README
+// See problem3.README
 
-// This loop reads in the array of input numbers
-var inputArray: [Int] = []
-while let inputNum = Int(readLine()!) {
-	inputArray.append(inputNum)
+import Foundation
+
+// This loop reads in the array of known X values
+var xData: [Double] = []
+while let inputNum = Double(readLine()!) {
+	xData.append(inputNum)
 }
 
-// Write your balancing code here.
+// This loop reads in the array of known Y values
+var yData: [Double] = []
+while let inputNum = Double(readLine()!) {
+    yData.append(inputNum)
+}
+
+
+// Write your linear regression code here
+
