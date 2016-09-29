9d052a09b1300e3a15c6c171e5230524c8ccd47b
gl3nnleblanc <gl3nnleblanc@gmail.com>
2016-09-29 13:45:53 -0700

    Solve problem1.swift

--- problem1.swift
+++ problem1.swift
@@ -4,4 +4,14 @@
 let data = [19, 524, 95, 627, 230, 960, 150, 880, 659, 724, 420, 215, 470, 787, 883, 269, 678, 718, 329, 355]
 
 let wantToFind = Int(readLine()!)!
-
+var didPrint = false
+for i in 0..<data.count {
+	if data[i] == wantToFind {
+		print(i)
+		didPrint = true
+		break
+	}
+}
+if didPrint == false {
+	print(-1)
+}
