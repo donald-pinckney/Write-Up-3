6a39071918cbc17c7353bceb8fcc80bcd4b9e2d8
Whyhow314 <whyhow3.141@gmail.com>
2016-09-30 03:51:59 +0000

    problem 2 works, problem 1 improved

--- problem1.swift
+++ problem1.swift
@@ -6,14 +6,11 @@ let data = [19, 524, 95, 627, 230, 960, 150, 880, 659, 724, 420, 215, 470, 787, 
 let wantToFind = Int(readLine()!)!
 
 var search = -1
-var loc = 0
 
-while loc < data.count {
-  if data[loc] == wantToFind {
-    search = loc
-    break
+for i in 0..<data.count {
+  let test = data[i]
+  if test == wantToFind {
+    search = i
   }
-  loc += 1
 }
-
 print(search)
