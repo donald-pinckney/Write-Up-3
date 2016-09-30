a518f2b92346870d4f34416889a7f987d414fe84
Whyhow314 <whyhow3.141@gmail.com>
2016-09-30 18:32:19 +0000

    Problem 3 solved

--- problem3.swift
+++ problem3.swift
@@ -17,3 +17,36 @@ while let inputNum = Double(readLine()!) {
 
 // Write your linear regression code here
 
+var products: [Double] = []
+var xSquares: [Double] = []
+var xSum: Double = 0
+var ySum: Double = 0
+var productsSum: Double = 0
+var xSquaresSum: Double = 0
+
+for i in 0..<xData.count {
+	let indProduct = xData[i] * yData[i]
+	products.append(indProduct)
+	let xSquared = xData[i] * xData[i]
+	xSquares.append(xSquared)
+}
+
+for j in 0..<xData.count {
+	xSum += xData[j]
+	ySum += yData[j]
+	productsSum += products[j]
+	xSquaresSum += xSquares[j]
+}
+
+let xAvg = xSum / Double(xData.count)
+let yAvg = ySum / Double(yData.count)
+let productsAvg = productsSum / Double(products.count)
+let xSquaresAvg = xSquaresSum / Double(xSquares.count)
+
+let m = (productsAvg - (xAvg * yAvg)) / (xSquaresAvg - (xAvg * xAvg))
+let b = yAvg - m * xAvg
+
+let mRounded = String.localizedStringWithFormat("%.4f", m)
+let bRounded = String.localizedStringWithFormat("%.4f", b)
+
+print("y = \(mRounded) * x + \(bRounded)")
