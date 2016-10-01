7180c0eb9823b4298e329826746c2ef37fba00ac
niraqw15 <niraqw@gmail.com>
2016-10-01 23:03:36 +0000

    Solve problem 2

--- problem2.swift
+++ problem2.swift
@@ -8,4 +8,31 @@ while let inputNum = Int(readLine()!) {
 
 // Write your balancing code here.
 
+var balPoint = -1
 
+//Checks every possible balance point by checking the sums of the values before and after it. checkIndex is the possible balcance point.
+for checkIndex in 1 ..< inputArray.count {
+	//print(checkIndex) //For testing
+
+	var sum1 = 0
+	var sum2 = 0
+
+	//Adds all of the values before and after the possible balane point.
+	for currentIndex in 0 ..< inputArray.count {
+		//print(currentIndex) //For testing
+
+		if currentIndex < checkIndex {
+			sum1 += inputArray[currentIndex]
+		} else {
+			sum2 += inputArray[currentIndex]
+		}
+	}
+
+	if sum1 == sum2 {
+		balPoint = checkIndex
+		break
+	}
+}
+
+//Prints final result.
+print(balPoint)
