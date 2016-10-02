46b28400a24edf534ef2dcb801435872776e6bbd
Ionatan Soule <ionatan98@gmail.com>
2016-10-02 10:06:29 -0700

    problem1.swift v2.0 alpha

--- problem1.swift
+++ problem1.swift
@@ -5,6 +5,8 @@ let data = [19, 524, 95, 627, 230, 960, 150, 880, 659, 724, 420, 215, 470, 787, 
 
 let wantToFind = Int(readLine()!)!
 
+//check to see if wantToFind in data
+//loop through values to find index
 if data.contains(wantToFind) {
 	for index in 0..<data.count {
 		if data[index] == wantToFind {
