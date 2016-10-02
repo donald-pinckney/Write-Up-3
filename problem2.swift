d429b6e3f254b18d07a58add0bd64475abbc3bf0
Ionatan Soule <ionatan98@gmail.com>
2016-10-01 19:17:10 -0700

    problem2.swift v1.0 beta

--- problem2.swift
+++ problem2.swift
@@ -9,3 +9,24 @@ while let inputNum = Int(readLine()!) {
 // Write your balancing code here.
 
 
+var sum = 0	//will become right sum
+
+for index in 0..<inputArray.count {
+	sum += inputArray[index]
+}
+
+var leftSum = 0	//to compare to sum (right sum)
+
+
+test: while true {
+	for index in 0..<inputArray.count {
+		sum -= inputArray[index]	//makes "sum" "right summ"
+		leftSum += inputArray[index]
+		if sum == leftSum {
+			print(index + 1)
+			break test
+		}
+	}
+	print(-1)
+	break
+}
