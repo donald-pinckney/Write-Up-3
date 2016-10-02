2aa6915cae90c640ada4b854568f931ac28d8d2c
Ionatan Soule <ionatan98@gmail.com>
2016-10-01 22:09:15 -0700

    problem3.swift v1.0 beta

--- problem3.swift
+++ problem3.swift
@@ -14,6 +14,52 @@ while let inputNum = Double(readLine()!) {
     yData.append(inputNum)
 }
 
-
 // Write your linear regression code here
 
+//average xData
+var sumx: Double = 0
+for x in xData {
+	sumx += x
+}
+let avgx = sumx / Double(xData.count)
+
+//average xData^2 -> xDatasq
+var xDatasq: [Double] = []
+var sumxsq: Double = 0
+for index in 0..<xData.count {
+	xDatasq.append( pow(xData[index], 2.0) )
+}
+for xsq in xDatasq {
+	sumxsq += xsq
+}
+let avgxsq = sumxsq / Double(xDatasq.count)
+
+//average yData
+var sumy: Double = 0
+for y in yData {
+	sumy += y
+}
+let avgy = sumy / Double(yData.count)
+
+//average xData*yData -> xyData
+var xyData: [Double] = []
+var sumxy: Double = 0
+for index in 0..<xyData.count {
+	xyData.append(xData[index] + yData[index])
+}
+for xy in xyData {
+	sumxy += xy
+}
+let avgxy = sumxy / Double(xyData.count)
+
+//compute m
+let m = (avgxy - avgx * avgy) / (avgxsq - pow(avgx, 2.0)) 
+
+//compute b
+let b = avgy - m * avgx
+
+//round to 4 decimals
+let mRounded = String.localizedStringWithFormat("%.4f", m)
+let bRounded = String.localizedStringWithFormat("%.4f", b)
+
+print("y = \(mRounded) * x + \(bRounded)")
