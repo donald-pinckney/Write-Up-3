3063922961b55220fb3548bf3ffd051c0d84cf36
Jacob Miller <jmmiller95616@gmail.com>
2016-10-04 23:43:11 +0000

    Completed Problem3

--- problem3.swift
+++ problem3.swift
@@ -2,6 +2,8 @@
 
 import Foundation
 
+import Foundation
+
 // This loop reads in the array of known X values
 var xData: [Double] = []
 while let inputNum = Double(readLine()!) {
@@ -14,6 +16,40 @@ while let inputNum = Double(readLine()!) {
     yData.append(inputNum)
 }
 
+var xyProduct: [Double] = []
+var xSquared: [Double] = []
 
 // Write your linear regression code here
+if xData.count == yData.count && xData.count > 0 {
+	let size = Double(xData.count)
 
+	var xSum: Double = 0
+	var ySum: Double = 0
+	var xySum: Double = 0
+	var xSquaredSum: Double = 0
+	for i in 0..<(xData.count) {
+		xySum = xySum + xData[i]*yData[i]
+		xyProduct.append(xData[i]*yData[i])
+
+		xSquaredSum = xSquaredSum + pow(xData[i],2)
+		xSquared.append(pow(xData[i],2))
+
+		xSum = xSum + xData[i]
+		ySum = ySum + yData[i]
+	}
+
+	let xAvg = xSum / size
+	let yAvg = ySum / size
+	let xyAvg = xySum / size
+	let xSquaredAvg = xSquaredSum / size
+
+	let m = (xyAvg - xAvg * yAvg)/(xSquaredAvg - pow(xAvg, 2))
+	let b = (yAvg) - m * (xAvg)
+
+	let mRounded = String.localizedStringWithFormat("%.4f", m)
+	let bRounded = String.localizedStringWithFormat("%.4f", b)
+
+	print("y = \(mRounded) * x + \(bRounded)")
+} else {
+	print("Invalid list formats.")
+}
