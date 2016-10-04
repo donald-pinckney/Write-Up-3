bb724a08ef0dc80fb35cfcbb1cda6aea7e6ef506
javaraser <darting.thoughts@gmail.com>
2016-10-04 00:30:12 +0000

    Problem1 Livy T

--- problem1.swift
+++ problem1.swift
@@ -4,4 +4,19 @@
 let data = [19, 524, 95, 627, 230, 960, 150, 880, 659, 724, 420, 215, 470, 787, 883, 269, 678, 718, 329, 355]
 
 let wantToFind = Int(readLine()!)!
+var found: Bool = false
+var i = 0
 
+while i < data.count{
+    if data[i] == wantToFind{
+        print(i)
+        found = true
+        break
+    }
+
+    i += 1
+}
+
+if !found{
+        print("-1")
+}
