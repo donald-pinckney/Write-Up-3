3687f6d30ce94a76d6910f65c53ad7bf56358380
Carter Luck <carter.luck@djusdk12ca.org>
2016-10-04 02:18:56 +0000

    Problems 1 2 and 3

--- problem2.swift
+++ problem2.swift
@@ -6,6 +6,31 @@ while let inputNum = Int(readLine()!) {
 	inputArray.append(inputNum)
 }
 
+var point = 0
+var j = 0
+var k = 0
+
+
 // Write your balancing code here.
 
+while j < inputArray.count {
+	var sum1 = 0
+	var sum2 = 0
+	for k in 0 ..< j {
+		sum1 += inputArray[k]
+	}
+	for k in j ..< inputArray.count {
+		sum2 += inputArray[k]
+	}
+	if sum1 == sum2 {
+		point = j
+		j = inputArray.count
+	}
+	j += 1
+}
 
+if point == 0 {
+	print("-1")
+} else {
+	print(point)
+}
