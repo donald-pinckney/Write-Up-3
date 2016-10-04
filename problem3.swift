9ec4bb92655e98533283b45cbb6cff0910591309
javaraser <darting.thoughts@gmail.com>
2016-10-04 00:31:56 +0000

    Problem 3 Begun Livy T

--- problem3.swift
+++ problem3.swift
@@ -14,6 +14,24 @@ while let inputNum = Double(readLine()!) {
     yData.append(inputNum)
 }
 
-
 // Write your linear regression code here
 
+//M
+//multiply xData by corresponding yData, store in  xyData THEN avg as xyCoAvg
+var xyData: [Double] = []
+for i in 0...(xData.count - 1){ //MULT xData value to corresponding yData, storing
+        xyData.append(xData[i] * yData[i])
+}
+for i in 0...(xData.count - 1){ //storing avg of xyData in xyCoAvg
+        var xyCoAvg += xyData[i]
+}
+var xyCoAvg /= xData.count
+
+//Avg of xdat and ydat SEPERATELY, multiply avgs together
+var xAvg = 0
+for i in 0...(xData.count - 1){
+        xAvg += xData[i]
+}
+
+print("xy Corresponging mult: /(xyCoData)")
+print("xy co Average: /(xyCoAvg)")
