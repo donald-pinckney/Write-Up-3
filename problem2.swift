7391f6142dd546c6d5297d768d04da08a2b3e860
Wentao Chen <wtchen2002@gmail.com>
2016-10-05 02:58:32 +0000

    Solve problem 2

--- problem2.swift
+++ problem2.swift
@@ -8,4 +8,26 @@ while let inputNum = Int(readLine()!) {
 
 // Write your balancing code here.
 
+var balanced = false
 
+for i in 0..<inputArray.count {
+	var sum1 = 0
+	var sum2 = 0
+
+	for j in 0..<i {
+		sum1 += inputArray[j]
+	}
+
+	for j in i..<inputArray.count {
+		sum2 += inputArray[j]
+	}
+
+	if sum1 == sum2 {
+		print(i)
+		balanced = true
+	}
+}
+
+if !balanced {
+	print(-1)
+}
