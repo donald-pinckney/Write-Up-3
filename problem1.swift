217d2bbabd1a316cb422c7d3c0d02ae0304ec62a
Nathan Solomon <nathansolomon1678@gmail.com>
2016-10-05 00:37:37 +0000

    problems 1-3 write up 3

--- problem1.swift
+++ problem1.swift
@@ -4,4 +4,10 @@
 let data = [19, 524, 95, 627, 230, 960, 150, 880, 659, 724, 420, 215, 470, 787, 883, 269, 678, 718, 329, 355]
 
 let wantToFind = Int(readLine()!)!
-
+var index = -1
+for i in 0...data.count - 1 {
+	if data [i] == wantToFind {
+		index = i
+	}
+}
+print (index)
--- problem2.swift
+++ problem2.swift
@@ -5,7 +5,24 @@ var inputArray: [Int] = []
 while let inputNum = Int(readLine()!) {
 	inputArray.append(inputNum)
 }
-
+var balanceIndex = -1
+	var sum = 0
+	var total = 0
 // Write your balancing code here.
+if inputArray.count > 1 {
+	
+	for i in 0...inputArray.count - 1 {
+		total += inputArray [i]
+	}
+	//finds the total of the numbers
+
+	for i in 0...inputArray.count - 1 {
+		sum += inputArray [i]
+		if Double(sum) == Double(total) / 2{
+			balanceIndex = i + 1
+		}
+	}
+}
 
 
+print (balanceIndex)
