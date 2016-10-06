b09ceec0a8e0387ea91975c8af2e2b914503708c
JMY2001 <justinmyeung@gmail.com>
2016-10-06 03:29:26 +0000

    solved all

--- problem2.swift
+++ problem2.swift
@@ -1,6 +1,7 @@
 // See problem2.README
 
 // This loop reads in the array of inputted positive integers
+
 var inputArray: [Int] = []
 while let inputNum = Int(readLine()!) {
 	inputArray.append(inputNum)
@@ -8,4 +9,23 @@ while let inputNum = Int(readLine()!) {
 
 // Write your balancing code here.
 
+var beforeSum = 0
+var afterSum = 0
+var balance = -1
 
+for testPoint in 0...inputArray.count {
+	for x in 0..<testPoint {
+		beforeSum += inputArray[x]
+			}
+	for y in testPoint..<inputArray.count {
+		afterSum += inputArray[y]
+	}
+	if beforeSum == afterSum && !(beforeSum == 0) {
+		balance = testPoint
+	}
+
+	beforeSum = 0
+	afterSum = 0
+}
+
+print(balance)
